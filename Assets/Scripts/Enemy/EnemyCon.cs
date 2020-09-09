615a6814731cb911253460bc60ec5eef17e8744d
IshiiTakto <syuukurasuta@ezweb.ne.jp>
2020-09-09 11:52:43 +0900

    Listから削除したときに出たエラーを削除

--- Assets/Scripts/Enemy/EnemyCon.cs
+++ Assets/Scripts/Enemy/EnemyCon.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
 public class EnemyCon : MonoBehaviour
 {
+    private bool _active = true;
+
     public int HP = 10;
     [SerializeField] float _speed = 1;
     Rigidbody2D _rig2d;
@@ -25,14 +28,15 @@ public class EnemyCon : MonoBehaviour
 
     public void EnemyUpdate()
     {
-        
+        if (!_active) return;
         if ((transform.position - _next).magnitude < 0.15f)
         {
             if (count == _root.Count)
             {
-                EnemyManager.Instance.DestroyEnemy(gameObject.GetComponent<EnemyCon>());
                 LevelManager.Instance.Damage();
-                Destroy(gameObject);
+                StartCoroutine(Des());
+                //Destroy(gameObject);
+                _active = false;
                 return;
             }
             _moveDirection = NextMove(transform.position);
@@ -40,6 +44,13 @@ public class EnemyCon : MonoBehaviour
 
         //_rig2d.velocity = _moveDirection * _speed;
         transform.position += _moveDirection * _speed;
+    }
+
+    private IEnumerator Des()
+    {
+        yield return null;
+        EnemyManager.Instance.DestroyEnemy(gameObject.GetComponent<EnemyCon>());
+        Destroy(gameObject);
     }
 
     public void Damage(int damage)
