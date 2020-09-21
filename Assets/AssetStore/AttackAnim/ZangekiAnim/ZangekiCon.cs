4de59a70608336b0ab33f0d0e1b33cd80c6e9671
IshiiTakto <syuukurasuta@ezweb.ne.jp>
2020-09-21 14:15:14 +0900

    PC切り替えのためすべてのアセットをpush

---
+++ Assets/AssetStore/AttackAnim/ZangekiAnim/ZangekiCon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZangekiCon : TowerAnimBase
+{
+    public override void AnimUpdate(float speed)
+    {
+        transform.position = new Vector3(transform.position.x + _moveVector.x, transform.position.y + _moveVector.y, transform.position.z + _moveVector.z);
+    }
+}
