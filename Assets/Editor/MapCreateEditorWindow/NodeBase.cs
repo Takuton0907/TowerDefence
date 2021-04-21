b87c42ae5ace8dcbedcbde412988f5e782ccc195
IshiiTakto <syuukurasuta@ezweb.ne.jp>
2021-04-21 18:30:56 +0900

    各ベースクラスの作成をしました　

---
+++ Assets/Editor/MapCreateEditorWindow/NodeBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+public class NodeBase : Node
+{
+
+}
---
+++ Assets/Editor/MapCreateEditorWindow/PortBase.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class PortBase : Port
+{
+    public PortBase(Orientation portOrientation, Direction portDirection, Capacity portCapacity, Type type) : base(portOrientation, portDirection, portCapacity, type)
+    {
+
+    }
+
+    public static new PortBase Create<TEdge>(Orientation orientation, Direction direction, Capacity capacity, Type type) where TEdge : Edge, new()
+    {
+        PortBase portBase = new PortBase(orientation, direction, capacity, type);
+        var edgeConnector = new EdgeConnector<TEdge>(new EdgeConnectorListener());
+        portBase.m_EdgeConnector = edgeConnector;
+        portBase.m_EdgeConnector.edgeDragHelper.draggedPort = portBase;
+        
+        return portBase;
+    }
+
+    public override void OnStartEdgeDragging()
+    {
+        Debug.Log("OnStart");
+        base.OnStartEdgeDragging();
+    }
+    public override void OnStopEdgeDragging()
+    {
+        Debug.Log("OnStop");
+        base.OnStopEdgeDragging();
+    }
+}
+
+public class EdgeConnectorListener : IEdgeConnectorListener
+{
+    public void OnDrop(GraphView graphView, Edge edge)
+    {
+        Debug.Log("OnDrop");
+    }
+
+    public void OnDropOutsidePort(Edge edge, Vector2 position)
+    {
+        Debug.Log("OnDropOut");
+    }
+}
