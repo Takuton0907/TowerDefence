774c15ebe9012df0893c41846368667a83fa9704
IshiiTakto <syuukurasuta@ezweb.ne.jp>
2021-04-27 19:56:49 +0900

    Listを初期化しました

--- Assets/Editor/MapCreateEditorWindow/MapData.cs
+++ Assets/Editor/MapCreateEditorWindow/MapData.cs
@@ -8,7 +8,7 @@ public class MapData : MonoBehaviour
     public GameObject _gameRoot { set; get; }
     public TextAsset _mapData { set; get; }
     public TileBase _load { set; get; }
-    public List<TileBase> _wall { set; get; }
+    public List<TileBase> _wall { set; get; } = new List<TileBase>();
     public TileBase _setTower { set; get; }
     public TileBase _start { set; get; }
     public TileBase _goal { set; get; }
